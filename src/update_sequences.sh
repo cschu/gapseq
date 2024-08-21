97a0df4f8743ee94b903082ad9adcc8a36498311
Christian Schudoma <christian.schudoma@embl.de>
2024-08-21 16:01:25 +0200

    add db location selection to update_sequences.sh

--- src/update_sequences.sh
+++ src/update_sequences.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 zenodoID=10047603
 
+# paths
+curdir=$(pwd)
+path=$(readlink -f "$0")
+dir=$(dirname "$path")
+export LC_NUMERIC="en_US.UTF-8"
+
 # taxonomy
 if [[ -z "$1" ]]; then
     taxonomy="Bacteria"
 else taxonomy=$1
 fi
 
-# paths
-curdir=$(pwd)
-path=$(readlink -f "$0")
-dir=$(dirname "$path")
-export LC_NUMERIC="en_US.UTF-8"
-seqpath=$dir/../dat/seq/$taxonomy
-seqpath_user=$dir/../dat/seq/$taxonomy/user
+if [[ -z "$2" ]]; then
+  seqpath_prefix=$dir/../dat/seq
+else
+  seqpath_prefix=$2
+fi
+
+seqpath=$seqpath_prefix/$taxonomy
+seqpath_user=$seqpath_prefix/$taxonomy/user
 mkdir -p $seqpath/rev $seqpath/unrev $seqpath_user $seqpath/rxn
 
 echo Checking updates for $taxonomy $seqpath
@@ -41,13 +48,13 @@ rm $zen_sums
 # check for mismatches of local md5sums and online version
 update_req=0
 while read -r md5sum filepath; do
-  if [ ! -f "$dir/../dat/seq/$filepath" ]; then
+  if [ ! -f "$seqpath_prefix/$filepath" ]; then
     update_req=1
     break
   fi
 
   # Calculate the MD5 sum of local file
-  calculated_md5=$(md5sum "$dir/../dat/seq/$filepath" | cut -d ' ' -f 1)
+  calculated_md5=$(md5sum "$seqpath_prefix/$filepath" | cut -d ' ' -f 1)
   
   # Compare the calculated MD5 sum with the one from the file
   if [ "$md5sum" != "$calculated_md5" ]; then
@@ -67,9 +74,9 @@ if [[ $update_req -eq 1 ]]; then
   
   # download+extract taxonomy-specific archive
   allseqs=$(mktemp)
-  wget -q -O "$dir/../dat/seq/$taxonomy.tar.gz" $url_zenodocurrent/files/$taxonomy.tar.gz
-  tar xzf $dir/../dat/seq/$taxonomy.tar.gz -C $dir/../dat/seq/
-  rm $dir/../dat/seq/$taxonomy.tar.gz
+  wget -q -O "$seqpath_prefix/$taxonomy.tar.gz" $url_zenodocurrent/files/$taxonomy.tar.gz
+  tar xzf $seqpath_prefix/$taxonomy.tar.gz -C $seqpath_prefix/
+  rm $seqpath_prefix/$taxonomy.tar.gz
   
   # extract rev/unrev/rxn archives
   tar xzf $seqpath/rev/sequences.tar.gz -C $seqpath/rev/
